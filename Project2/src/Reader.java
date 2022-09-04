996bd033eb51842f723a3571bd0a58e914630725
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-09-04 15:54:30 -0600

    Create Reader.java

---
+++ Project2/src/Reader.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+
+public class Reader {
+    
+  
+    /**
+     * @return str: String
+     * 
+     * This method reads a string from the console input.
+     */
+    public static String read() {   
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter a string: ");
+        
+        String str = scanner.nextLine();
+        scanner.close();
+
+        return str;
+    }
+}
